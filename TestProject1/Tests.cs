e9a875afb0107bcf89ed465a3c53e62c51540971
Michael A. Newman <michael.newman@centricconsulting.com>
2018-04-03 15:21:00 -0400

    New test with expected exception, to see how the build and Team City reacts

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -11,5 +11,26 @@ namespace TestProject1
         {
             Assert.True(true);
         }
+
+        [Test]
+        public void ExcetptionTest()
+        {
+            var rng = new Random();
+            var numerator = rng.Next();
+            var denominator = 0;
+            var testObject = new TestObject();
+
+            var result = Assert.Throws<DivideByZeroException>(() => testObject.DivideIntegers(numerator, denominator));
+
+            Assert.AreEqual("Attempted to divide by zero.", result.Message);
+        }
+    }
+
+    public class TestObject
+    {
+        public int DivideIntegers(int numerator, int denominator)
+        {
+            return numerator / denominator;
+        }
     }
 }
